b29fe298d4584bc7ad31d9c39697ede1d1b00713
Thomas Scelsi <tscelsi@evenergi.com>
2023-02-08 16:57:08 +0100

    feat: change home caption text

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ export default function IndexPage() {
         <div className="max-w-3xl mb-32 lg:p-0 p-1 flex flex-col justify-center items-center gap-6">
           <p className="text-whyte text-center font-black text-4xl lg:text-6xl">Australia's first multi-site sporting odds API.</p>
           <div className="w-3/5 my-4 bg-gradient-to-r from-[#68E3F9] via-[#F55A9B] to-[#6F6FDD] h-px"></div>
-          <p className="text-whyte text-center font-light">The OddsChaser API allows you to easily retrieve odds for sporting events in near real-time from 12 Australian bookies (and increasing!)</p>
+          <p className="text-whyte text-center font-light">The OddsChaser API allows you to easily retrieve odds for sporting events in near real-time from multiple Australian bookies at once</p>
           <div className="flex gap-x-4">
             <Link href="/docs/overview">
               <Button size="large">Read the docs</Button>
